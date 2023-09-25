fbb1f2624cef20f3d95dbd42e9d0794d90b2e4cd
emzilia <giblinhm@miamioh.edu>
2023-09-25 08:53:56 -0400

    added way to check api key usage

--- translate.py
+++ translate.py
@@ -10,7 +10,9 @@ def arg_check():
             "Usage: tran se <word of phrase>\n\t"
             "Translates the input text from Spanish to English\n"
             "       tran es <word or phrase>\n\t"
-            "Translates the input text from English to Spanish"
+            "Translates the input text from English to Spanish\n\n"
+            "       tran usage\n\t"
+            "Shows DeepL API key usage"
         )
         sys.exit(1)
     elif len(sys.argv) > 20:
@@ -18,7 +20,9 @@ def arg_check():
             "words at a time.")
         sys.exit(1)
 
-    if sys.argv[1] == 'se':
+    if sys.argv[1] == 'usage':
+        flag = "USAGE"
+    elif sys.argv[1] == 'se':
         flag = "EN-US"
     elif sys.argv[1] == 'es':
         flag = "ES"
@@ -29,6 +33,16 @@ def arg_check():
 
     return flag
    
+# checks the usage of the DeepL free API calls
+def api_check(translator):
+    usage = translator.get_usage()
+    if usage.any_limit_reached:
+        print("This DeepL API key has reached its usage limit.")
+        sys.exit(1)
+    else:
+        print(f"Usage: {usage.character.count} / {usage.character.limit}")
+        sys.exit(1)
+
 # requires a DeepL API key, which are free to acquire, however you must
 # provide billing information. Currently set as an env variable
 def init_deepl():
@@ -37,8 +51,11 @@ def init_deepl():
     return translator
 
 # based on the flag set by the first argument, text is translated as single
-# words or as strings built by successive script args
+# words or as strings built by successive script args, API usage can
+# also be shown
 def trans_input(translator, flag):
+    if flag == "USAGE":
+        api_check(trans)
     if len(sys.argv) == 3:
         newword = translator.translate_text(sys.argv[2],
                                                 target_lang=flag)
@@ -55,6 +72,9 @@ if __name__ == "__main__":
     flag = arg_check()
 
     trans = init_deepl()
+
+    if flag == "USAGE":
+        api_check(trans)
 
     output = trans_input(trans, flag)
 
