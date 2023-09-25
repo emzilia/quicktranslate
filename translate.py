14ef88f4ad397cabfa8b385689780ddb1b44732d
emzilia <giblinhm@miamioh.edu>
2023-09-25 11:30:46 -0400

    simplified logic, remembered quotes are a thing

--- translate.py
+++ translate.py
@@ -7,17 +7,25 @@ import deepl, os, sys
 def arg_check():
     if len(sys.argv) == 1:
         print(
-            "Usage: tran se <word of phrase>\n\t"
-            "Translates the input text from Spanish to English\n"
-            "       tran es <word or phrase>\n\t"
-            "Translates the input text from English to Spanish\n\n"
+            "Usage: tran se \"<word of phrase>\"\n\t"
+            "Translates the quoted input text from Spanish to English\n"
+            "       tran es \"<word or phrase>\"\n\t"
+            "Translates the quoted input text from English to Spanish\n"
             "       tran usage\n\t"
-            "Shows DeepL API key usage"
+            "Shows DeepL API key usage (no quotes)"
         )
         sys.exit(1)
-    elif len(sys.argv) > 20:
-        print("Error: This program doesn't support more than 20 "
-            "words at a time.")
+    elif len(sys.argv) == 2:
+        print(
+            "Error: This program requires at least two arguments to "
+            "function"
+        )
+        sys.exit(1)
+    elif len(sys.argv) > 3:
+        print(
+            "Error: This program doesn't support more than one block "
+            "of quotes at a time"
+        )
         sys.exit(1)
 
     if sys.argv[1] == 'usage':
@@ -27,12 +35,21 @@ def arg_check():
     elif sys.argv[1] == 'es':
         flag = "ES"
     else:
-        print("Error: First arg must be 'se' or 'es' for translation to "
-            "English or Spanish respectively")
+        print(
+            "Error: First arg must be 'se' or 'es' for translation to "
+            "English or Spanish respectively"
+        )
         sys.exit(1)
 
     return flag
    
+# requires a DeepL API key, which are free to acquire, however you must
+# provide billing information. Currently set as an env variable
+def init_deepl():
+    auth_key = os.environ["DEEPL_API_KEY"]
+    translator = deepl.Translator(auth_key)
+    return translator
+
 # checks the usage of the DeepL free API calls
 def api_check(translator):
     usage = translator.get_usage()
@@ -43,13 +60,6 @@ def api_check(translator):
         print(f"Usage: {usage.character.count} / {usage.character.limit}")
         sys.exit(1)
 
-# requires a DeepL API key, which are free to acquire, however you must
-# provide billing information. Currently set as an env variable
-def init_deepl():
-    auth_key = os.environ["DEEPL_API_KEY"]
-    translator = deepl.Translator(auth_key)
-    return translator
-
 # based on the flag set by the first argument, text is translated as single
 # words or as strings built by successive script args, API usage can
 # also be shown
@@ -57,25 +67,15 @@ def trans_input(translator, flag):
     if flag == "USAGE":
         api_check(trans)
     if len(sys.argv) == 3:
-        newword = translator.translate_text(sys.argv[2],
-                                                target_lang=flag)
+        newword = translator.translate_text(
+            sys.argv[2],
+            target_lang=flag
+        )
         return newword
-    else:
-        phrase = ''
-        for word in sys.argv[2:]:
-            phrase += word + ' '
-        newphrase = translator.translate_text(phrase, 
-                                              target_lang=flag)
-        return newphrase
 
 if __name__ == "__main__":
     flag = arg_check()
-
     trans = init_deepl()
-
-    if flag == "USAGE":
-        api_check(trans)
-
     output = trans_input(trans, flag)
 
     print(output)
