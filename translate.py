159dcda02681e1af6e28b51fc8f1cc9aa8ca39c6
emzilia <em@zilia.dev>
2025-03-20 17:13:28 -0400

    fixed arg_check logic, updated comments

--- translate.py
+++ translate.py
@@ -13,13 +13,10 @@ except ImportError:
 # to spanish or 'se' for spanish to english, 'usage' shows api key usage.
 def arg_check(trans) -> str:
     output = ''
+    option = ''
+    text = ''
     try:
         option = sys.argv[1]
-    except IndexError:
-        print_help()
-        sys.exit(1)
-    try:
-        text: str = sys.argv[2]
     except IndexError:
         print_help()
         sys.exit(1)
@@ -29,15 +26,24 @@ def arg_check(trans) -> str:
     elif len(sys.argv) == 2:
         if option == '-h' or option == '--help':
             print_help()
-        if option == '-u':
+        elif option == '-u':
             print_usage(trans)
+        else:
+            print_help()
     elif len(sys.argv) == 3:
+        try:
+            text: str = sys.argv[2]
+        except IndexError:
+            print_help()
+            sys.exit(1)
         if option == '-se':
             flag: str = "EN-US"
             output = trans_input(trans, flag, text)
         elif option == '-es':
             flag: str = "ES"
             output = trans_input(trans, flag, text)
+        else:
+            print_help()
     elif len(sys.argv) > 3:
         print(
             "Error: This script doesn't support more than one block "
@@ -47,9 +53,7 @@ def arg_check(trans) -> str:
 
     return output
    
-# requires a DeepL api key, which is free to acquire. checks api usage and
-# closes if it's reached its limit, shows usage data if flag is set
-# set as an env variable
+# requires a DeepL api key, which is free to acquire, set as an env variable
 def init_deepl():
     auth_key: str = os.environ["DEEPL_API_KEY"]
     translator = deepl.Translator(auth_key)
@@ -63,7 +67,7 @@ def trans_input(translator, flag, text) -> str:
     )
     return output
 
-# checks the usage of the DeepL api calls
+# prints the usage of the DeepL api calls
 def print_usage(translator) -> None:
     usage = translator.get_usage()
     if usage.any_limit_reached:
@@ -77,6 +81,7 @@ def print_usage(translator) -> None:
     )
     sys.exit(1)
 
+# prints help dialogue
 def print_help() -> None:
     print(
         "Usage: tran [option] <word or phrase to translate in quotes>\n"
@@ -84,7 +89,7 @@ def print_help() -> None:
         "or show API key usage.\n"
         "  Options:\n"
         "    -se\t\t"
-        "    Translates the quoted input text from English to Spanish\n"
+        "    Translates the quoted input text from Spanish to English\n"
         "    -es\t\t"
         "    Translates the quoted input text from English to Spanish\n"
         "    -u, --usage\t"
